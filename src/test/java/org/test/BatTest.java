8df7d0c901e2df402240f927200e58100c7d8a94
rajvexr <thers3001@gmail.com>
2022-08-09 15:32:15 +0100

    added all tests

--- src/test/java/org/test/BatTest.java
+++ src/test/java/org/test/BatTest.java
@@ -1,6 +1,7 @@
 package org.test;
 
 import org.example.Bat;
+import org.example.FlyingRat;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,7 +12,39 @@ public class BatTest {
     public void get_batName(){
         Bat bat = new Bat();
         assertEquals("flying creature", bat.getName());
+    }
+    @Test
+    public void get_batAge(){
+        Bat bat2 = new Bat();
+        assertEquals(3, bat2.getAge());
+    }
+    @Test
+    public void get_batDead(){
+        Bat bat3 = new Bat();
+        assertEquals(false, bat3.isDead());
+    }
+    @Test
+    public void get_batFood(){
+        Bat bat4 = new Bat();
+        assertEquals("flying creature eats night-flying insects", bat4.eat(), "the bat is not hungry");
+    }
 
+    @Test
+    public void get_batTakeFlight(){
+        Bat bat5 = new Bat();
+        assertEquals("flying creature takes flight", bat5.takeFlight(), "the bat is not taking flight");
+    }
+
+    @Test
+    public void get_batFlight(){
+        Bat bat6 = new Bat();
+        assertEquals("flying creature is flying at night", bat6.flight(), "the bat is not flying");
+    }
+
+    @Test
+    public void get_batLanding(){
+        Bat bat7 = new Bat();
+        assertEquals("flying creature is beginning to land", bat7.landing(), "the bat is not landing");
     }
 
 }
--- src/test/java/org/test/FlyingRatTest.java
+++ src/test/java/org/test/FlyingRatTest.java
@@ -13,6 +13,41 @@ public class FlyingRatTest {
         FlyingRat flyingrat = new FlyingRat();
         assertEquals("Rat", flyingrat.getName(), "this is not the name");
     }
+    @Test
+    public void get_flyingRatAge(){
+        FlyingRat frat2 = new FlyingRat();
+        assertEquals(6, frat2.getAge(), "this age is not the name");
+    }
+    @Test
+    public void get_flyingRatDead(){
+        FlyingRat frat3 = new FlyingRat();
+        assertEquals(true, frat3.isDead(), "the flyingRat is not dead");
+    }
+
+    @Test
+    public void get_flyingRatFood(){
+        FlyingRat frat4 = new FlyingRat();
+        assertEquals("Rat eats insects", frat4.eat(), "the flyingRat is not dead");
+    }
+
+    @Test
+    public void get_flyingRatTakeFlight(){
+        FlyingRat frat5 = new FlyingRat();
+        assertEquals("Rat is beginning to take flight", frat5.takeFlight(), "the flyingRat is not taking flight");
+    }
+
+    @Test
+    public void get_flyingRatFlight(){
+        FlyingRat frat6 = new FlyingRat();
+        assertEquals("Rat is flying", frat6.flight(), "the flyingRat is not flying");
+    }
+
+    @Test
+    public void get_flyingRatLanding(){
+        FlyingRat frat7 = new FlyingRat();
+        assertEquals("Rat is landing", frat7.landing(), "the flyingRat is not landing");
+    }
+
 
 
 }
