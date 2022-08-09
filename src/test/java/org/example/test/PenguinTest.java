c00b6e7ea9a2e68e25273239400529d619c1562c
rajvexr <thers3001@gmail.com>
2022-08-09 17:21:29 +0100

    adding code coverage update

--- src/test/java/org/example/test/PenguinTest.java
+++ src/test/java/org/example/test/PenguinTest.java
@@ -10,6 +10,25 @@ public class PenguinTest {
     @Test
     public void get_PenguinFood() {
         Penguin penguin = new Penguin();
-        assertEquals("Bob eats krill, squids, and fishes", penguin.eat());
+        assertEquals("Bob eats krill, squids, and fishes", penguin.eat(), "the penguin is not eating");
     }
+
+    @Test
+    public void get_PenguinPoop(){
+        Penguin penguin2 = new Penguin();
+        assertEquals("the animal is pooping", penguin2.poop(), "the penguin is not pooping");
+    }
+
+    @Test
+    public void get_PenguinMove(){
+        Penguin penguin3 = new Penguin();
+        assertEquals("the animal is moving", penguin3.move(), "the penguin is not moving");
+    }
+
+    @Test
+    public void get_PenguinCheckAge(){
+        Penguin penguin4 = new Penguin();
+        assertEquals(9, penguin4.getAge());
+    }
+
 }
